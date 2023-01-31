ebd726b3ce1ca2fd85ccece94bd0f3fd2cbf32d5
radenmasabdul <abdul.tamsis@gmail.com>
2023-02-01 00:08:17 +0700

    Get All Users

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Frame = () => {
+  const [user, setUser] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = async () => {
+    const response = await axios.get("https://api.github.com/users");
+    setUser(response.data);
+  };
+
+  const seacrhUser = async (e) => {
+    e.preventDefault();
+    await axios.get(`https://api.github.com/users/${id}`);
+  };
+
   return (
     <>
-      <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form class="space-y-6" action="#">
+      <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <form className="space-y-6" onSubmit={seacrhUser}>
           <div>
             <input
-              type="email"
-              name="email"
-              id="email"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              type="text"
+              name="text"
+              id="text"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
               placeholder="Enter Username"
               required
             />
           </div>
           <button
             type="submit"
-            class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Search
           </button>
+
+          <p>Showing user for "{user.login}"</p>
+
+          <div
+            tabIndex={0}
+            className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+          >
+            <div className="collapse-title text-xl font-medium">
+              {user.login}
+            </div>
+            <div className="collapse-content">
+              {user.map((login) => (
+                <p key={login.id}>{login.login}</p>
+              ))}
+            </div>
+          </div>
         </form>
       </div>
     </>
